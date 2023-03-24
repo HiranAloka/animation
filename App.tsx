59bd70b83c2d9fa7c003a46992217a8a9f5b9f84
alokaLimark <116781808+alokaLimark@users.noreply.github.com>
2023-03-24 13:44:30 +0530

    added the changes

--- App.tsx
+++ App.tsx
@@ -11,46 +11,106 @@ import {transformer} from './metro.config';
 
 const App = () => {
   let valueRight = useRef(new Animated.Value(0)).current;
+  let valueLeft = useRef(new Animated.Value(0)).current;
 
   const windowHeight = Dimensions.get('window').height;
+  const windowWidth = Dimensions.get('window').width;
 
   const positionInterpolate = valueRight.interpolate({
     inputRange: [0, 1, 2],
     outputRange: [0, -windowHeight + 100, 0],
     extrapolate: 'clamp',
   });
+
+  const positionInterpolateDown = valueLeft.interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, windowHeight - 100, 0],
+    extrapolate: 'clamp',
+  });
   const moveBall = () => {
-    Animated.sequence([
-      Animated.timing(valueRight, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-      Animated.timing(valueRight, {
-        toValue: 2,
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-    ]).start(() => {});
+    Animated.parallel([
+      Animated.sequence([
+        Animated.timing(valueRight, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(valueRight, {
+          toValue: 2,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ]),
+      Animated.sequence([
+        Animated.timing(valueLeft, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+        Animated.timing(valueLeft, {
+          toValue: 2,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start();
   };
 
   return (
-    <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-      <Animated.View
-        style={[
-          {
-            width: 100,
-            height: 100,
-            borderRadius: 100 / 2,
-            backgroundColor: 'red',
-            transform: [{translateY: positionInterpolate}],
-          },
-        ]}
-      />
-      <TouchableOpacity onPress={moveBall}>
-        <Text>Hey Click ME</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+      <View
+        style={{
+          height: windowHeight,
+          width: windowWidth,
+          flexDirection: 'row',
+        }}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}>
+          <Animated.View
+            style={[
+              {
+                width: 100,
+                height: 100,
+                borderRadius: 100 / 2,
+                backgroundColor: 'red',
+                transform: [{translateY: positionInterpolate}],
+              },
+            ]}
+          />
+        </View>
+        <View
+          style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+          <Animated.View
+            style={[
+              {
+                width: 100,
+                height: 100,
+                borderRadius: 100 / 2,
+                backgroundColor: 'red',
+                transform: [{translateY: positionInterpolateDown}],
+              },
+            ]}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: windowWidth,
+          height: windowHeight,
+        }}>
+        <TouchableOpacity onPress={moveBall}>
+          <Text>Hey Click ME</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 export default App;
