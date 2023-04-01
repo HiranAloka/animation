2f3ea2ca29644e3d294078537b5a43621f6086c3
alokaLimark <116781808+alokaLimark@users.noreply.github.com>
2023-04-01 10:42:26 +0530

    added the changes

--- App.tsx
+++ App.tsx
@@ -1,56 +1,144 @@
-import React, {useRef, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {
-  View,
-  Animated,
-  TouchableOpacity,
-  Easing,
-  Text,
-  Dimensions,
-} from 'react-native';
-import {transformer} from './metro.config';
+  createStackNavigator,
+  TransitionSpecs,
+  HeaderStyleInterpolators,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
+import React from 'react';
+import {Easing, StatusBar, StyleSheet, Text, View} from 'react-native';
+import Home from './src/screen/Home';
+import ScreenA from './src/screen/ScreenA';
+import ScreenB from './src/screen/ScreenB';
+import ScreenC from './src/screen/ScreenC';
+import ScreenD from './src/screen/ScreenD';
+import ScreenE from './src/screen/ScreenE';
 
-const App = () => {
-  let valueRight = useRef(new Animated.Value(0)).current;
+const Stack = createStackNavigator();
 
-  const windowHeight = Dimensions.get('window').height;
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
 
-  const positionInterpolate = valueRight.interpolate({
-    inputRange: [0, 1, 2],
-    outputRange: [0, -windowHeight + 100, 0],
-    extrapolate: 'clamp',
-  });
-  const moveBall = () => {
-    Animated.sequence([
-      Animated.timing(valueRight, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-      Animated.timing(valueRight, {
-        toValue: 2,
-        duration: 1000,
-        useNativeDriver: true,
-      }),
-    ]).start(() => {});
-  };
+const closeConfig = {
+  animation: 'timing',
+  config: {
+    duration: 200,
+    easing: Easing.linear,
+  },
+};
 
-  return (
-    <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-      <Animated.View
-        style={[
+const customTransition = {
+  gestureEnabled: true,
+  gestureDirection: 'horizontal',
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  cardStyleInterpolator: ({current, next, layouts}) => {
+    return {
+      cardStyle: {
+        transform: [
           {
-            width: 100,
-            height: 100,
-            borderRadius: 100 / 2,
-            backgroundColor: 'red',
-            transform: [{translateY: positionInterpolate}],
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }),
           },
-        ]}
+          {
+            rotate: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: ['180deg', '0deg'],
+            }),
+          },
+          {
+            scale: next
+              ? next.progress.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [1, 0.7],
+                })
+              : 1,
+          },
+        ],
+      },
+      opacity: current.opacity,
+    };
+  },
+};
+
+const AppStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: 'horizontal',
+      }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="ScreenA"
+        component={ScreenA}
+        options={{
+          gestureDirection: 'vertical',
+          transitionSpec: {
+            open: config,
+            close: closeConfig,
+          },
+          cardStyleInterpolator: CardStyleInterpolators.forModalPresentationIOS,
+        }}
       />
-      <TouchableOpacity onPress={moveBall}>
-        <Text>Hey Click ME</Text>
-      </TouchableOpacity>
-    </View>
+      <Stack.Screen
+        name="ScreenB"
+        component={ScreenB}
+        options={{
+          transitionSpec: {
+            open: config,
+            close: closeConfig,
+          },
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      />
+      <Stack.Screen
+        name="ScreenC"
+        component={ScreenC}
+        options={{
+          ...customTransition,
+        }}
+      />
+      <Stack.Screen
+        name="ScreenD"
+        component={ScreenD}
+        options={{
+          gestureDirection: 'vertical',
+          cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+        }}
+      />
+      <Stack.Screen
+        name="ScreenE"
+        component={ScreenE}
+        options={{
+          gestureDirection: 'vertical-inverted',
+          cardStyleInterpolator:
+            CardStyleInterpolators.forRevealFromBottomAndroid,
+        }}
+      />
+    </Stack.Navigator>
   );
 };
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+};
+
 export default App;
